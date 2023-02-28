a653083245d5f4f04b467bf6007ffa9d64c0e455
knn654 <kaua.nascimento@fatec.sp.gov.br>
2023-02-28 02:22:07 -0300

    Adição da listagem de processo

--- src/controller/KillController.java
+++ src/controller/KillController.java
@@ -1,5 +1,45 @@
 package controller;
 
-public class KillController {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
+public class KillController {
+	
+	private String os() {
+		String nome = System.getProperty("os.name");
+		return nome;
+		
+	}
+	
+	public void listaProcessos() {
+		String nome = System.getProperty("os.name");
+		if(nome.contains("Windows")) {
+			String processo = "TASKLIST /FO TABLE";
+			try {
+				Process p = Runtime.getRuntime().exec(processo);
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					System.out.println(linha);
+					linha = buffer.readLine();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			
+		}
+		
+	}
+	
+	public void mataPid() {
+		
+	}
+	
+	public void mataNome() {
+		
+	}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,12 @@
 package view;
 
+import controller.KillController;
+
 public class Main {
+	public static void main(String args[]) {
+		KillController killcontroller = new KillController();
+		
+		killcontroller.listaProcessos();	
+	}
 
 }
